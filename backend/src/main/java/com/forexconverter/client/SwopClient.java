36de6947fe34fc36d9b3d344654343304a0004a7
Vova Smyshliaev <1913904+vforvova@users.noreply.github.com>
2026-02-17 19:21:34 +0200

    refactor: simplify SwopClient constructor

--- backend/src/main/java/com/forexconverter/client/SwopClient.java
+++ backend/src/main/java/com/forexconverter/client/SwopClient.java
@@ -2,7 +2,6 @@ package com.forexconverter.client;
 
 import com.forexconverter.config.SwopClientProperties;
 import com.forexconverter.dto.SwopRateResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -12,22 +11,7 @@ public class SwopClient {
 
   private final RestClient restClient;
 
-  @Autowired
-  public SwopClient(RestClient.Builder builder, SwopClientProperties properties) {
-    SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-    requestFactory.setConnectTimeout(properties.timeout());
-    requestFactory.setReadTimeout(properties.timeout());
-
-    this.restClient =
-        builder
-            .baseUrl(properties.baseUrl())
-            .defaultHeader("Authorization", "ApiKey " + properties.apiKey())
-            .requestFactory(requestFactory)
-            .build();
-  }
-
-  // For testing with MockWebServer
-  SwopClient(SwopClientProperties properties) {
+  public SwopClient(SwopClientProperties properties) {
     SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
     requestFactory.setConnectTimeout(properties.timeout());
     requestFactory.setReadTimeout(properties.timeout());
